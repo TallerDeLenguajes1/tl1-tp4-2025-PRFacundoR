9cf6c58c47b9d24eb71af965de7eddd6d1245fc3
FacundPeraltaRomero <facundoperalta217@gmail.com>
2025-04-15 16:05:41 -0300

    con errores

--- ejercicio1.c
+++ ejercicio1.c
@@ -2,25 +2,75 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-
+#define C 1000
 
 struct {
     int TareaID;//Numérico autoincremental comenzando en 1000
     char *Descripcion; //
     int Duracion; // entre 10 – 100
-    }typedef Tarea;
+}typedef Tarea;
 
-    struct Nodo{
+ struct {
     Tarea T;
     struct Nodo *Siguiente;
-    };
-    
-    typedef struct Nodo* Lista;
+}typedef Nodo;
+
+
+
+
+    Nodo* creacion(){
+
+        return NULL;
+    }
+
+    Nodo* CrearNodo(int *duracion, char descripcion[], int *tamanio,int *incremento){
+        Nodo* tareaPendiente= (Nodo*)malloc(sizeof(Nodo));
+
+        tareaPendiente->T.Duracion=*duracion;
+
+        tareaPendiente->T.Descripcion=(char*)malloc(*tamanio*sizeof(char)+1);
+
+        strcpy(tareaPendiente->T.Descripcion,descripcion);
+
+        tareaPendiente->T.TareaID=*incremento;
+
+        tareaPendiente->Siguiente=NULL;
+        
+        return tareaPendiente;
+    }
+
+
 
 int main()
 {
+    srand(time(NULL));
+    int incremento;
+    int numeroTPend;
+    int duracion;
+    int tamanio;
+    char descripcion[C];
+    Nodo *Inicio=creacion();
 
+    printf("Ingrese el numero de tareas que quieres dar`\n");
+    scanf("%d",&numeroTPend);
 
+    for (int i = 0; i < numeroTPend; i++)
+    {
+        incremento=1000+i;
+        printf("\nIngrese la duracion de la tarea entre 10 y 100\n");
+        scanf("%d",&duracion);
+
+        printf("Ingrse la descripcion de la tarea\n");
+        gets(descripcion);
+        tamanio=strlen(descripcion);
+
+        Nodo *tareaPendiente= CrearNodo(&duracion, descripcion, &tamanio,&incremento);
+
+    }
+    
+    
+    
+    
     
     return 0;
 }
