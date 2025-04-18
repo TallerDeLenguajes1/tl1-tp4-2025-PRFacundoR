f38dd9106f062ecf073efa24f5c79bf7ef393bd6
FacundoPeralta <facundoperalta217@gmail.com>
2025-04-18 11:30:07 -0300

    listo

--- ejercicio1.c
+++ ejercicio1.c
@@ -69,25 +69,120 @@ typedef struct Nodo {
 
     }
 
+    void mostrarListadoTareas(Nodo **Lista){
+
+        while (*Lista!=NULL)
+        {
+
+            printf("El id de la tarea es: %d\n",(*Lista)->T.TareaID);
+            printf("La descripcion de la tarea es: \n");
+            fflush(stdin);
+            puts((*Lista)->T.Descripcion);
+            fflush(stdin);
+            printf("La duracion de la lista es: %d\n",(*Lista)->T.Duracion);
+            printf("\n");
+
+
+            *Lista=(*Lista)->Siguiente;
+        }
+        
+
+
+    }
+
+    void consultaTareas(Nodo **listaI, Nodo **listaF,int*id,char palabraClave[]){
+        int consulta;
+        printf("Oprima 1 si quiere consultar por Id de la tarea, oprima 2 si quiere consultar por una palabra clave, otro numero si quiere salir de la operacion\n");
+        scanf("%d",&consulta);
+        while (consulta<3 && consulta>0)
+        {
+            
+        
+        
+        switch (consulta)
+        {
+        case 1:
+
+            while (*listaI || *listaF)
+            {
+                
+                if ((*listaI)->T.TareaID==*id)
+                {
+                    printf("La tarea pertenece a tarea pendiente");
+                }else if((*listaF)->T.TareaID==*id){
+                    printf("La tarea pertenece a tarea realizada");
+                }else{
+                    printf("No existe tarea con ese id");
+                }
+                
+
+
+                *listaI=(*listaI)->Siguiente;
+                *listaF=(*listaF)->Siguiente;
+            }
+            
+ 
+            break;
+        
+        case 2:
+
+        while (*listaI || *listaF)
+        {
+            
+            if (strstr((*listaI)->T.Descripcion,palabraClave)!=NULL)
+            {
+                printf("La tarea pertenece a tarea pendiente");
+            }else if(strstr((*listaF)->T.Descripcion,palabraClave)!=NULL){
+                printf("La tarea pertenece a tarea realizada");
+            }else{
+                printf("No existe tarea con esa descripcion");
+            }
+            
+
+
+            *listaI=(*listaI)->Siguiente;
+            *listaF=(*listaF)->Siguiente;
+        }
+
+
+
+           
+            break;
+
+        
+        }
+        printf("Oprima 1 para otra consulta por id de tarea, 2 para consultar por palabra clave, otro numero para salir\n");
+        scanf("%d", &consulta);
+    }
+        
+
+
+    }
+
+
 
 int main()
 {
     srand(time(NULL));
-    int incremento=1000;
+    int id=1000;
     int numeroTPend;
     int duracion;
     int tamanio=0;
-    char descripcion[C];
-    Nodo *Inicio=creacion();
-    Nodo *Fin=creacion();
+    char descripcion[C]; 
+    char palabraClave[C];
+    Nodo *TareasPendientes=creacion();
+    Nodo *TareasRealizadas=creacion();
     int final=0;
+    int PasajeDeNodo;
+    int idTarea;
+
    
     printf("Ponga 0 si quiere agregar una tarea o 1 si quiere terminar de agregar tareas \n");
     scanf("%d",&final);
     
     while(final==0)
     {
-        incremento+=1;
+        
         printf("\nIngrese la duracion de la tarea entre 10 y 100\n");
         scanf("%d",&duracion);
 
@@ -97,20 +192,43 @@ int main()
         gets(descripcion);
         tamanio=strlen(descripcion);
 
-        Nodo *tareaPendiente= CrearNodo(&duracion, descripcion, &tamanio,&incremento);
-        insertarNodo(&Inicio, tareaPendiente);
-        
+        Nodo *tareaPendiente= CrearNodo(&duracion, descripcion, &tamanio,&id);
+        insertarNodo(&TareasPendientes, tareaPendiente);
+        id+=1;
         printf("Desea seguir agregando tareas ponga 0 si es asi o 1 si quiere finalizar\n");
         
         scanf("%d",&final);
       
     }
     
+    printf("Si quieres pasar un nodo de tarea pendiente a tarea realizada, oprima 1(uno), sino apriete otro numero\n");
+    scanf("%d",&PasajeDeNodo);
+    while (PasajeDeNodo==1)
+    {
+        printf("Ingrese el id(de 1000(mil) para adelante) de la tarea a cambiar de lista\n");
+        scanf("%d",&idTarea);
+        
 
+        pasajeDeNodos(&TareasPendientes, &TareasRealizadas, &idTarea);
 
+        printf("Si quieres pasar un nodo de tarea pendiente a tarea realizada, oprima 1(uno), sino apriete otro numero\n");
+        scanf("%d",&PasajeDeNodo);
+    }
+    
+
+    printf("Lista de Tareas Pendientes:\n");
+    mostrarListadoTareas(&TareasPendientes);
+    fflush(stdin);
+    printf("Lista de Tareas Realizadas:\n");
+    mostrarListadoTareas(&TareasRealizadas);
 
     
-    
+   
+    printf("Ingrese la palabra clave para buscar en las tareas: \n");
+    fflush(stdin);
+    gets(palabraClave);
+    fflush(stdin);
+    consultaTareas(&TareasPendientes, &TareasRealizadas, &id, palabraClave);
     
     
     return 0;
